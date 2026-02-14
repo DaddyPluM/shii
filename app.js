f8802d4177f684671e8b6ab37edeb65c6e2536cc
DaddyPluM <103752309+DaddyPluM@users.noreply.github.com>
2026-02-14 23:47:41 +0100

    new file:   api.js 	modified:   app.js 	modified:   index.html 	modified:   style.css

--- app.js
+++ app.js
@@ -1,17 +1,88 @@
-async function fetchPosts(){
-    const response = await fetch ("https://jsonplaceholder.typicode.com/posts?_limit=5");
-    const posts = await response.json();
+import { api } from './api.js';
 
-    const container = document.getElementById('posts-container');
+const studentsDiv = document.getElementById("students");
+const saveBtn = document.getElementById("saveBtn");
 
-    container.innerHTML = "";
-    posts.forEach(post => {
-        const div = document.createElement("div");
-        div.innerClassName = "post";
-        div.innerHTML = " <h2>$(post.title)</h2> <p>$(post.body)</p>"
-        container.appendChild(div);
-    });
+let localStudents = [];
+
+saveBtn.addEventListener("click", saveStudent);
+
+async function loadStudents() {
+
+  const students = await api.getStudents();
+
+  localStudents = students.map(s => ({
+    id: s.id,
+    name: s.name,
+    department: "N/A"
+  }));
+
+  displayStudents();
 }
 
-document.getElementById("loadPostBtn")
-    .addEventListener("click", fetchPosts);
+function displayStudents() {
+
+  studentsDiv.innerHTML = localStudents.map(({ id, name, department }) => `
+    <div class="card">
+      <strong>${id} - ${name}</strong> (${department})
+      <br/>
+      <button onclick="editStudent(${id})">Edit</button>
+      <button onclick="deleteStudent(${id})">Delete</button>
+    </div>
+  `).join("");
+}
+
+async function saveStudent() {
+
+  const id = document.getElementById("studentId").value;
+  const name = document.getElementById("studentName").value;
+  const department = document.getElementById("department").value;
+
+  if (!id) {
+
+    const newStudent = await api.createStudent({ name, department });
+
+    localStudents.push({
+      id: newStudent.id,
+      name,
+      department
+    });
+
+  } else {
+
+    await api.updateStudent(id, { name, department });
+
+    const index = localStudents.findIndex(s => s.id == id);
+    localStudents[index] = { id: Number(id), name, department };
+
+  }
+
+  clearForm();
+  displayStudents();
+}
+
+window.editStudent = function(id) {
+
+  const student = localStudents.find(s => s.id === id);
+
+  document.getElementById("studentId").value = student.id;
+  document.getElementById("studentName").value = student.name;
+  document.getElementById("department").value = student.department;
+}
+
+window.deleteStudent = async function(id) {
+
+  await api.deleteStudent(id);
+
+  localStudents = localStudents.filter(s => s.id !== id);
+
+  displayStudents();
+}
+
+function clearForm() {
+  document.getElementById("studentId").value = "";
+  document.getElementById("studentName").value = "";
+  document.getElementById("department").value = "";
+}
+
+loadStudents();
